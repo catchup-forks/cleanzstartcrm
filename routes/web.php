a9949908e1ec4b9935375b716bc56e42066d7576
Niels Drost <nielsdrost7@gmail.com>
2018-11-23 13:25:04 +0100

    quick commit with LaraTrust

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,49 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::group(['prefix' => 'admincp', 'middleware' => ['role:owner']], function() {
+	Route::get('/', function () {
+		return view('welcome');
+	});
+    Route::get('/manage', ['middleware' => ['permission:manage-admins'], 'uses' => 'AdminController@manageAdmins']);
 });
+
+
+
+
+Route::group(['middleware' => ['web']], function() {
+	// Authentication Routes...
+	$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+	$this->post('login', 'Auth\LoginController@login');
+	$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+	// Registration Routes...
+	$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+	$this->post('register', 'Auth\RegisterController@register');
+
+	// Password Reset Routes...
+	$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+	$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Include static app constants
 require_once app_path() . '/Constants.php';
